71a3343cf4bef691201586cb7e8a26838b0fe406
NF RIDOY <nfridoy@gmail.com>
2024-01-24 00:21:55 +0600

    [feat](useAuth): useAuth hook setup

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Login from './pages/Login/Login.jsx';
+import AuthProvider from './providers/AuthProvider/AuthProvider.jsx';
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,8 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
