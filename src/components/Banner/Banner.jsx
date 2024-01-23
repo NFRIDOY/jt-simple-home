f8a186cc4b0b648729f87ec73a2bcbcb98d364f9
NF RIDOY <nfridoy@gmail.com>
2024-01-23 22:48:39 +0600

    [feat](Banner): login btn

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+
+const Banner = () => {
+    return (
+        <div className="object-cover h-48 w-full relative">
+            <div>
+                <img className="object-cover h-[500px] w-full border-2 " src="https://images.unsplash.com/photo-1497005367839-6e852de72767?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Banner" />
+            </div>
+            <Link to={"/login"} className="btn btn-primary text-white absolute top-[240px] right-[100px] md:right-[200px] lg:right-[350px]">
+                <button >
+                    Login
+                </button>
+            </Link>
+        </div>
+    );
+};
+
+export default Banner;
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Login from './pages/Login/Login.jsx';
 
 const router = createBrowserRouter([
   {
