7843747b8c3e067f99cf732cfefe11d174e454c0
NF RIDOY <nfridoy@gmail.com>
2024-01-23 22:57:39 +0600

    [feat](Btn): login btn

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
             <div>
                 <img className="object-cover h-[500px] w-full border-2 " src="https://images.unsplash.com/photo-1497005367839-6e852de72767?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Banner" />
             </div>
-            <Link to={"/login"} className="btn btn-primary text-white absolute top-[240px] right-[100px] md:right-[200px] lg:right-[350px]">
+            <Link to={"/login"} className="trans btn btn-primary hover:btn-accent font-bold text-white absolute top-[240px] right-[100px] md:right-[200px] lg:right-[350px]">
                 <button >
                     Login
                 </button>
