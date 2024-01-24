6b001099beadebc0b15ec56fb36c8b318abeb5d9
NF RIDOY <nfridoy@gmail.com>
2024-01-24 14:03:10 +0600

    [fix](Signup): form

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -3,7 +3,22 @@ import useAuth from '../../hooks/useAuth';
 
 const Signup = () => {
 
-    const { test } = useAuth();
+    const { user, setUser, loading, setLoading, createUserEmailPass, signInEmailPass, googleSignIn, logOut } = useAuth();
+
+    const handleSignup = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const name = form.name.value;
+
+        const userObj = {
+            email,
+            password,
+            name
+        }
+        console.log(userObj);
+    }
 
     return (
         <div className="mx-auto w-fit h-screen lg:p-16">
@@ -13,11 +28,26 @@ const Signup = () => {
                     Sign Up
                 </h3>
             </div>
-            <div className="relative flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border border-2 border-gray-400">
+            <form 
+            onSubmit={handleSignup}
+            className="relative flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border border-2 border-gray-400" >
 
                 <div className="flex flex-col gap-4 p-6">
                     <div className="relative h-11 w-full min-w-[200px]">
                         <input
+                            id='name'
+                            name='name'
+                            className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                            placeholder=" " />
+                        <label
+                            className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                            Name
+                        </label>
+                    </div>
+                    <div className="relative h-11 w-full min-w-[200px]">
+                        <input
+                            id='email'
+                            name='email'
                             className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                             placeholder=" " />
                         <label
@@ -27,6 +57,8 @@ const Signup = () => {
                     </div>
                     <div className="relative h-11 w-full min-w-[200px]">
                         <input
+                            id='password'
+                            name='password'
                             className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                             placeholder=" " />
                         <label
@@ -57,9 +89,9 @@ const Signup = () => {
                     </div> */}
                 </div>
                 <div className="p-6 pt-0">
-                    <button to={"/signup"}
+                    <button 
                         className="block w-full select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                        type="button">
+                        type="submit">
                         Sign up
                     </button>
                     <p className="flex justify-center mt-6 font-sans text-sm antialiased font-light leading-normal text-inherit">
@@ -70,7 +102,7 @@ const Signup = () => {
                         </Link>
                     </p>
                 </div>
-            </div>
+            </form>
         </div>
     );
 };
