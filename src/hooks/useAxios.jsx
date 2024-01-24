6c87c9581876506d21c23a68f84d2c7e249f30ff
NF RIDOY <nfridoy@gmail.com>
2024-01-25 01:17:52 +0600

    [Deploy](firebase)

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const instance = axios.create({
+    baseURL: 'https://jt-simple-home-server.vercel.app',
     // baseURL: 'http://localhost:5000',
-    baseURL: 'http://localhost:5000',
     // withCredentials: true
 });
 const useAxios = () => {
